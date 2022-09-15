dfa1dc4bb0a6d10dd16e60181db13af9a87eb0f1
Alexei Ulanov <lucky-stees@yandex.ru>
2022-09-15 16:53:26 +0300

    Change error message to show that tiktok is temporarily unavalible

--- bot/bot.go
+++ bot/bot.go
@@ -72,7 +72,7 @@ func InitBot() {
 				err = tiktok.Handle(update, bot)
 				if err != nil {
 					db.DRIVER.LogError("Couldn't handle a tiktok request", utils.GetTelegramUserString(update.Message.From), err.Error())
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Sorry, something went wrong while processing your request. Please try again later")
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Tiktok requests are currently unavailable in the bot, we're investigating the issue and will inform you when it's finally going back. Thank you for your patience!")
 					msg.ReplyToMessageID = update.Message.MessageID
 					bot.Send(msg)
 					return
